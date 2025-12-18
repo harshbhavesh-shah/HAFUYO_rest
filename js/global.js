d61a9782e70f8f5f478cb21f383cf30ee9e391a2
Alam Yotzan Aguilar Chavez <alam-yotzan.aguilar-chavez@etu.univ-amu.fr>
2025-12-18 15:03:04 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -10,17 +10,6 @@ if(header) {
     });
 }
 
-function toggleMenu() {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.getElementById('nav');
-    
-    if (hamburger && nav) {
-        hamburger.classList.toggle('active');
-        nav.classList.toggle('active');
-    }
-}
-
-
 const backToTopBtn = document.getElementById('backToTop');
 if(backToTopBtn){
     backToTopBtn.addEventListener('click', () => {
@@ -74,14 +63,3 @@ document.getElementById('backToTop').addEventListener('click', () => {
     });
 });
 //menu.js code end
-
-//THIS CODE WAS ORIGINALLY ON footer.js file
-const header = document.querySelector('header');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
